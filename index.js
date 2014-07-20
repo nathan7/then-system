b8c53d72b2053bc7920c7f1b5e1f38b52e1ae4c5
Nathan Zadoks <nathan@nathan7.eu>
2014-07-20 17:31:17 +0200

    expose stdin and child

--- index.js
+++ index.js
@@ -6,39 +6,47 @@ var Promise = require('promise')
 
 module.exports =
 function os(bin, args, opts) {
-  return new Promise(function(resolve, reject) {
-    if (!Array.isArray(args)) {
-      opts = args
-      args = []
-    }
+  var child
+    , ret = new Promise(function(resolve, reject) {
+        if (!Array.isArray(args)) {
+          opts = args
+          args = []
+        }
 
-    var name = bin
-    if (Array.isArray(bin)) {
-      name = bin.join(' ')
-      args = bin.concat(args)
-      bin = args.shift()
-    }
+        var name = bin
+        if (Array.isArray(bin)) {
+          name = bin.join(' ')
+          args = bin.concat(args)
+          bin = args.shift()
+        }
 
-    opts = opts
-      ? Object.create(opts)
-      : {}
-    opts.stdio = [process.stdin, process.stdout, 'pipe']
+        opts = opts
+          ? Object.create(opts)
+          : {}
+        opts.stdio = [(opts.stdio && opts.stdio[0]) || process.stdin, (opts.stdio && opts.stdio[1]) || process.stdout, 'pipe']
 
-    var child = spawn(bin, args, opts)
-      , stderr = sprom(stderr)
+        child = spawn(bin, args, opts)
+        var stderr = sprom(stderr)
 
-    if (!opts.quiet) child.stderr.pipe(process.stderr)
+        if (!opts.quiet) child.stderr.pipe(process.stderr)
 
-    child.on('error', reject)
-    child.on('exit', function(code, signal) {
-      if (code === 0 && !signal) return resolve()
-      stderr.then(function(stderr) {
-        var err = errorCode(code, signal)
-        err.message += ': ' + stderr
-        err.stderr = stderr
+        child.on('error', reject)
+        child.on('exit', function(code, signal) {
+          if (code === 0 && !signal) return resolve()
+          stderr.then(function(stderr) {
+            var err = errorCode(code, signal)
+            err.message += ': ' + stderr
+            err.stderr = stderr
 
-        reject(err)
+            reject(err)
+          })
+        })
       })
-    })
-  })
+
+  if (child) {
+    ret.child = child
+    ret.stdin = child.stdin
+  }
+
+  return ret
 }
